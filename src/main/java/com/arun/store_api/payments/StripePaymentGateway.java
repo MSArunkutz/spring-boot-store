39ed66ba74142d2c4b6daf4e4a2aa5f66a7178e8
MSArunkutz <arunms911@gmail.com>
2025-09-09 20:08:48 +0530

    Fixing the orderid issue during checkout

--- src/main/java/com/arun/store_api/payments/StripePaymentGateway.java
+++ src/main/java/com/arun/store_api/payments/StripePaymentGateway.java
@@ -32,7 +32,7 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntent(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -75,6 +75,12 @@ public class StripePaymentGateway implements PaymentGateway {
         );
         var paymentIntent = (PaymentIntent) stripeObject;
         return Long.valueOf(paymentIntent.getMetadata().get("order_id"));
+    }
+
+    private static SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
+                .build();
     }
 
 
